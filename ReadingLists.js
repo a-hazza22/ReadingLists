55fec23732de10b0deace2ea00ce2c0d6435320b
a-hazza22 <7483088@ogrenci.bilecik.edu.tr>
2025-08-25 19:21:49 +0300

    اضافة دوال لاستخراج قوائم الكتب وعرضها + امر الطباعه

--- ReadingLists.js
+++ ReadingLists.js
@@ -32,3 +32,23 @@ goodreadsInfo.wantToRead = addNewBook(goodreadsInfo.wantToRead, {
   title: "Looking for Alaska",
   author: "John Green",
 });
+// هذه دالة لعرض جميع المعلومات في وحدة التحكم.
+const showGoodreadsInfo = (info) => {
+    // يتم استخراج قوائم الكتب من الكائن info
+  const currentlyReading = info.currentlyReading;
+  const wantToRead = info.wantToRead;
+
+  console.log("Currently Reading:");
+  for (let book of currentlyReading) {
+    // يطبع عنوان الكتاب والمؤلف بتنسيق معين.
+    console.log(`${book.title} by ${book.author}`);
+  }
+  // اوامر الطباعه
+  console.log();
+  console.log("Want to Read:");
+  for (let book of wantToRead) {
+    console.log(`${book.title} by ${book.author}`);
+  }
+};
+
+showGoodreadsInfo(goodreadsInfo);
