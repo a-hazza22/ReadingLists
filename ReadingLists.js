f9188c3e3a2fb410813a4562d84cacb65b2d4da8
Abdullah Hazza <161160104+a-hazza22@users.noreply.github.com>
2025-08-25 19:03:59 +0300

    القسم الاول من الكود
    يحتوي على دالتين واحده خاصة بالكتب التي يتم قراءتها والاخرى لتخزين الكتب

---
+++ ReadingLists.js
@@ -0,0 +1,19 @@
+// دالة لإضافة كتب جديدة
+const goodreadsInfo = {
+ // لتخزين الكتب التي يتم قراءتها حاليًا
+  currentlyReading: [
+    {
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+    },
+  ],
+  // هذه مصفوفة لتخزين الكتب التي يريد المستخدم قراءتها
+
+  wantToRead: [
+    {
+      title: "The Art of Language Invention",
+      author: "David Peterson",
+    },
+  ],
+};
+
